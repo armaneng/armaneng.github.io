4bc98e29bb043c3f92c3fbff5470431066d3351e
armanezz <arkhamknight0776@gmail.com>
2021-11-21 14:39:22 +0330

    added toggle button js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// implement toggle button functionality
+
+let toggleButton = document.querySelector(".toggle-button");
+let menuNav = document.querySelector(".menu-nav");
+let backdrop = document.querySelector(".backdrop");
+
+toggleButton.addEventListener("click", toggleMenuNav);
+
+function toggleMenuNav() {
+    if (toggleButton.classList.contains("toggle-button--close")) {
+        closeMenuNav();
+    }
+    else {
+        openMenuNav();
+    }
+}
+
+backdrop.addEventListener("click", closeMenuNav);
+
+function openMenuNav() {
+    toggleButton.classList.add("toggle-button--close");
+    backdrop.style.display = "block";
+    menuNav.style.display = "flex";
+    setTimeout(function() {
+        backdrop.classList.add("backdrop--opened");
+        menuNav.classList.add("menu-nav--opened");
+    }, 10);
+}
+
+function closeMenuNav() {
+    toggleButton.classList.remove("toggle-button--close");
+    backdrop.classList.remove("backdrop--opened");
+    setTimeout(function() {
+        backdrop.style.display = "none";
+    }, 200);
+    menuNav.classList.remove("menu-nav--opened");
+}
